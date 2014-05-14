f7833dcbe8d9d5b581c899e4d8a7a9421dc6bc17
zhao-li- <1216186168@qq.com>
2014-05-14 16:57:50 +0800

    delete demo

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,10 +10,6 @@ angular
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/myroute', {
         templateUrl: 'views/myroute.html',
         controller: 'MyrouteCtrl'
       })
